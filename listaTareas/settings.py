80c65722d662282695a86b453c32cbbf5e326ba1
Alexis Ramos <alexleaderhere@gmail.com>
2023-06-28 21:52:35 -0300

    changes

--- listaTareas/settings.py
+++ listaTareas/settings.py
@@ -125,3 +125,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Directorio de archivos estáticos recopilados para producción
+STATIC_ROOT = BASE_DIR / 'static'
